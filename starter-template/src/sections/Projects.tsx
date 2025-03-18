e6ebd17328b94d9071f41e9e77476fe3d462effe
Vanshika <vanshikaduggal13@gmail.com>
2025-03-18 23:01:27 +0530

    tape completed

--- starter-template/src/sections/Projects.tsx
+++ starter-template/src/sections/Projects.tsx
@@ -2,7 +2,10 @@ import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
 import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
 import Image from "next/image";
-
+import CheckCircleIcon from '@/assets/icons/check-circle.svg'
+import ArrowUpRightIcon from '@/assets/icons/arrow-up-right.svg'
+import grainImage from '@/assets/images/grain.jpg'
+import { SectionHeader } from "@/components/SectionHeader";
 
 const portfolioProjects = [
   {
@@ -45,57 +48,71 @@ const portfolioProjects = [
 
 export const ProjectsSection = () => {
   return (
-    <div className="py-12">
-      <div className="container mx-auto px-4">
+    <section className="pb-16 lg:py-24">
+      <div className="container">
+        <SectionHeader eyebrow="Real-world Results" title=" Featured Projects" description=" See how I transformed concepts into engaging digital experiences."/>
         <div className="flex justify-center">
           <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text text-center">
             Real-world Results
           </p>
         </div>
-        <h2 className="font-serif text-3xl text-center mt-6">
+        <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">
           Featured Projects
         </h2>
-        <p className="text-center text-white/60 mt-4">
+        <p className="text-center md:text-lg lg:text-xl text-white/60 mt-4 max-w-md mx-auto">
           See how I transformed concepts into engaging digital experiences.
         </p>
 
-        <div className="flex flex-col mt-10">
+        <div className="flex flex-col mt-10 gap-20 md:mt-20">
           {portfolioProjects.map((project) => (
-            <div key={project.title} className="bg-gray-800 rounded-3xl z-0 overflow-hidden after:z-10 relative after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:outline-offset-2 after:rounded-3xl after:outline-white/20 p-8">
-              <div className="flex">
-                <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex  gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
-                  <span>{project.company}</span>
-                  <span>&bull;</span>
-                  <span>{project.year}</span>
+            <div key={project.title} className="bg-gray-800 rounded-3xl z-0 overflow-hidden after:z-10 relative after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:outline-offset-2 after:rounded-3xl md:pt-12 md:px-10 after:outline-white/20
+             p-8 pt-8 after:pointer-events-none lg:pt:16 lg:px-20">
+              <div className="absolute inset-0 -z-10 opacity-5" style={{
+                backgroundImage: `url(${grainImage.src})`
+              }}></div>
+
+              <div className="lg:grid lg:grid-cols-2 lg:gap-16">
+
+                <div className="lg:pb-16">
+                  <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex  gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
+                    <span>{project.company}</span>
+                    <span>&bull;</span>
+                    <span>{project.year}</span>
+                  </div>
+
+                  <h3 className="font-serif text-2xl mt-2 md:text-4xl md:mt-5 md:font-bold" >{project.title}</h3>
+                  <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
+                  <ul className="flex flex-col gap-4 mt-4 md:mt-5">
+                    {project.results.map((result, index) => (
+                      <li className="flex gap-2 text-sm text-white/50 md:text-base ">
+                        <CheckCircleIcon className="size-5 ms:size-6" />
+                        <span>{result.title}</span>
+                      </li>
+                    ))}
+                  </ul>
+                  <a
+                    href={project.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
+                      <span>Visit Live Site</span>
+                      <ArrowUpRightIcon className="size-4" />
+                    </button>
+                  </a>
+                </div>
+                <div className="relative">
+                  <Image
+                    src={project.image}
+                    alt={project.title}
+                    className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none"
+                  />
                 </div>
               </div>
-              <h3 className="font-serif text-2xl mt-2">{project.title}</h3>
-              <hr className="border-t-2 border-white/5 mt-4" />
-              <ul className="list-disc list-inside text-gray-700 mb-4">
-                {project.results.map((result, index) => (
-                  <li>
-                    <span>{result.title}</span>
-                  </li>
-                ))}
-              </ul>
-              <a
-                href={project.link}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <button className="bg-gradient-to-r from-emerald-400 to-sky-400 text-white px-4 py-2 rounded-lg hover:opacity-90 transition">
-                  View Live Site
-                </button>
-              </a>
-              <Image
-                src={project.image}
-                alt={project.title}
-                className="rounded-lg mb-4"
-              />
             </div>
           ))}
         </div>
       </div>
-    </div>
+    </section>
   );
 };
