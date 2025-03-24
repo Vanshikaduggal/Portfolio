2a4d388bd8f25857b540f89807b2eafe2c3c1981
Vanshika <vanshikaduggal13@gmail.com>
2025-03-24 22:46:21 +0530

    about-section completed

--- starter-template/src/sections/About.tsx
+++ starter-template/src/sections/About.tsx
@@ -45,34 +45,53 @@ const toolboxItems = [
 const hobbies = [
   {
     title: 'Dancing',
-    emojie: '💃'
+    emojie: '💃',
+    left:'5%',
+    top:'5%',
   },
   {
     title: 'Gaming',
-    emojie: '🎮'
+    emojie: '🎮',
+    left:'50%',
+    top:'5%',
+    
   },
   {
     title: 'Painting',
-    emojie: '🎨'
+    emojie: '🎨',
+    left:'10%',
+    top:'35%',
   },
   {
     title: 'Hiking',
-    emojie: '🚞'
+    emojie: '🚞',
+    left:'35%',
+    top:'40%',
   },
   {
     title: 'Fitness',
-    emojie: '🏋️‍♂️'
+    emojie: '🏋️‍♂️',
+    left:'70%',
+    top:'45%',
   },
   {
     title: 'Reading',
-    emojie: '📖'
+    emojie: '📖',
+    left:'5%',
+    top:'70%',
+  },
+  {
+    title: 'Music',
+    emojie: '🎵',
+    left:'45%',
+    top:'70%',
   },
 
 ]
 
 export const AboutSection = () => {
   return (
-    <div className="py-20">
+    <div className="py-20 lg:py-28">
       <div className="container">
         <SectionHeader
           eyebrow="About Me"
@@ -80,26 +99,33 @@ export const AboutSection = () => {
           description="Learn more about who I am, what I do ,and what inspires me." />
 
         <div className="mt-20 flex flex-col gap-8">
-          <Card className="h-[320px]">
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-5 lg:grid-cols-3">
+          <Card className="h-[320px] md:col-span-2 lg:col-span-1">
             <CardHeader title="My Reads" description="Explore the books shaping my perspective."/>
-            <div className="w-40 mx-auto mt-8">
+            <div className="w-40 mx-auto mt-2 md:mt-0">
             <Image src={bookImage} alt="Book cover" className=""/>
             </div>
           </Card>
 
-          <Card className="h-[320px] p-0">
+          <Card className="h-[320px] md:col-span-3 lg:col-span-2">
             <CardHeader title="My Toolbox" description="Explore the technologies and tools I use to craft exceptional digital experiences."
-            className="px-6 pt-6"/>
-            <ToolboxItem toolboxItems={toolboxItems} className="mt-6" itemsWrapperClassName=" -translate-x-1/2"/>
+            className=""/>
+            <ToolboxItem toolboxItems={toolboxItems} className="" itemsWrapperClassName=" -translate-x-1/2"/>
             <ToolboxItem toolboxItems={toolboxItems} className="mt-6"/>
             
           </Card>
+          </div>
 
-          <Card>
-            <CardHeader title="Beyond the Code" description="Explore my interests and hobbies beyond the digital realm."/>
-            <div>
+          <div className="grid gap-8 grid-cols-1 md:grid-cols-5 lg:col-span-3">
+          <Card className="h-[320px] p-0 flex flex-col md:col-span-3 lg:col-span-3">
+            <CardHeader title="Beyond the Code" description="Explore my interests and hobbies beyond the digital realm." className="px-6 py-6"/>
+            <div className="relative flex-1">
               {hobbies.map(hobby => (
-                <div key={hobby.title} className="inline-flex gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5">
+                <div key={hobby.title} className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute"
+                style={{
+                  left:hobby.left,
+                  top:hobby.top,
+                }}>
                   <span className="font-medium text-gray-950">{hobby.title}</span>
                   <span>{hobby.emojie}</span>
                 </div>
@@ -107,11 +133,14 @@ export const AboutSection = () => {
             </div>
           </Card>
 
-          <Card>
-            <Image src={mapImage} alt="map" />
-            <Image src={smileMemoji} alt="simling memoji" />
+          <Card className="h-[320px] p-0 relative col-span-2 lg:col-span-2">
+            <Image src={mapImage} alt="map"  className="h-full w-full object-cover"/>
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:outline-offset-2 after:rounded-full after:outline-gray-950/30">
+            <Image src={smileMemoji} alt="simling memoji" className="size-20"/>
+            </div>
           </Card>
 
+          </div>
 
         </div>
       </div>
--- starter-template/src/sections/Contact.tsx
+++ starter-template/src/sections/Contact.tsx
@@ -1,3 +1,17 @@
+import ArreowRightIcon from '@/assets/icons/arrow-up-right.svg'
+
 export const ContactSection = () => {
-  return <div>Contact Section</div>;
+  return (
+    <div className='py-16 pt-14'>
+      <div className='container'>
+        <div className='bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center'>
+        <h2 className='font-serif text-2xl'>Let's create something amazing together</h2>
+        <p className='text-sm mt-2'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi, assumenda.</p>
+        <button className='text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 mt-8'>
+          <span className='font-semibold'>Contact Me</span>
+          <ArreowRightIcon className="size-4"/>
+        </button>
+        </div>
+      </div>
+    </div>)
 };
